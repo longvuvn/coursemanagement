26551eea76d0e8f70d47a11ba93b1472224acc17
longvuvn <nguyenlongvuvlog@gmail.com>
2025-07-07 16:08:42 +0700

    Update AdminServiceImpl.java

--- src/main/java/com/example/coursemanagement/services/impl/AdminServiceImpl.java
+++ src/main/java/com/example/coursemanagement/services/impl/AdminServiceImpl.java
@@ -52,7 +52,7 @@ public class AdminServiceImpl implements AdminService {
         }
         if (adminRepository.existsByEmail(adminDTO.getEmail().trim())) {
             throw new DuplicateResourceException("Email không hợp lệ");
-
+        }
         if(adminRepository.existsByPhoneNumber(adminDTO.getPhoneNumber().trim())){
             throw new DuplicateResourceException("Số điện thoại không hợp lệ");
         }
